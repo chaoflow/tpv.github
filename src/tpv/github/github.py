ca0e7305f71f6b08fde3b6e522c313e0ce08279d
Jonas Hoersch <coroa@online.de>
2013-11-17 16:15:24 +0100

    github: listing of issues, creation of repos

--- src/tpv/github/github.py
+++ src/tpv/github/github.py
@@ -1,5 +1,8 @@
 from requests import request
-import os, ConfigParser
+import os
+import ConfigParser
+import re
+import itertools
 
 from metachao import aspect
 from metachao import classtree
@@ -21,7 +24,11 @@ config = ConfigParser.ConfigParser()
 config.read(os.path.join(os.environ['HOME'], ".ghconfig"))
 
 
-def github_request(method, urlpath):
+def merge_dicts(*dicts):
+    return dict(itertools.chain(*(d.iteritems() for d in dicts)))
+
+
+def github_request(method, urlpath, data=None):
     """Request `urlpath` from github using authentication from config
 
     Arguments:
@@ -31,21 +38,145 @@ def github_request(method, urlpath):
 
     return request(method, URL_BASE + urlpath,
                    auth=(config.get("github", "user"),
-                         config.get("github", "token")))
+                         config.get("github", "token")),
+                   data=data)
+
+
+def github_request_paginated(method, urlpath):
+    while urlpath:
+        req = github_request(method, urlpath)
+        for elem in req.json():
+            yield elem
+
+        urlpath = None
+        if "Link" in req.headers:
+            m = re.search('<(https[^>]*)>; rel="next"', req.headers["Link"])
+            if m:
+                urlpath = m.group(1)[len(URL_BASE):]
+
+
+def github_request_length(urlpath):
+    req = github_request("GET", urlpath + "?per_page=1")
+    m = re.search('<https[^>]*[?&]page=(\d+)[^>]*>; rel="last"',
+                  req.headers["Link"])
+    if m:
+        return int(m.group(1))
+    else:
+        return 0
+
+
+class GhResource(dict):
+    @property
+    def url_template(self):
+        raise NotImplemented()
+
+    def __init__(self, parent, data = None, **kwargs):
+        self._parent = parent
+        for k, v in kwargs.iteritems():
+            setattr(self, "_" + k, v)
+
+        if data is None:
+            data = github_request("GET", self.url_template
+                                         .format(**kwargs).json())
+        self.merge(data)
+
+
+class GhIssue(object):
+    """Issue of some repository
+    """
+    def __init__(self, owner, repo, number, data=None):
+        """
+        Arguments:
+        - `owner`:
+        - `repo`:
+        - `number`:
+        - `data`:
+        """
+        self._owner = owner
+        self._repo = repo
+        self._number = number
+
+        if data is None:
+            data = github_request("GET", "/repos/{}/{}/issues/{}"
+                                         .format(owner, repo, number)).json()
+        self._data = data
+
+    def iterkeys(self):
+        return self._data.iterkeys()
+    __iter__ = iterkeys
+
+    def itervalues(self):
+        return self._data.itervalues()
+
+    def iteritems(self):
+        return self._data.iteritems()
+
+    def keys(self):
+        return self._data.keys()
+
+    def values(self):
+        return self._data.values()
+
+    def items(self):
+        return self._data.items()
+
+    def __getitem__(self, key):
+        return self._data[key]
 
 
 class GhRepoIssues(object):
     """The issues of some repository
     """
 
-    def __init__(self, owner, repo):
+    def __init__(self, parent):
         """
         Arguments:
-        - `owner`:
-        - `repo`:
+        - `ghrepo`: the parent GhRepo instance
         """
-        self._owner = owner
-        self._repo = repo
+        self._owner = parent._owner
+        self._repo = parent._repo
+
+    def _get_all_issues(self):
+        urlpath = "/repos/{}/{}/issues".format(self._owner, self._repo)
+        return itertools.chain(github_request_paginated("GET", urlpath),
+                               github_request_paginated("GET", urlpath + "?state=closed"))
+
+    def __len__(self):
+        return len(self.keys())
+
+    def iterkeys(self):
+        """Enumerate issues of the repository"""
+        return (x["number"] for x in self._get_all_issues())
+
+    __iter__ = iterkeys
+
+    def keys(self):
+        return list(self.iterkeys())
+
+    def itervalues(self):
+        return (x[1] for x in self.iteritems())
+
+    def values(self):
+        return list(self.itervalues())
+
+    def iteritems(self):
+        return ((x["number"], GhIssue(self._owner, self._repo, x["number"], x))
+                for x in self._get_all_issues())
+
+    def items(self):
+        return list(self.iteritems())
+
+    def __getitem__(self, number):
+        """Return the GhIssue object for issue number `number`"""
+        # Check if `repo` is a valid repo/user
+        req = github_request("GET", "/repos/{}/{}/issues/{}"
+                                    .format(self._owner, self._repo, number))
+        if "200" not in req.headers["status"]:
+            raise KeyError("Issue with number `{}` does not exist in repo `{}/{}`."
+                           .format(number, self._owner, self._repo))
+
+        # Return GhOwnerRepos object
+        return GhIssue(self._owner, self._repo, number, req.json())
 
 
 @classtree.instantiate
@@ -65,12 +196,13 @@ class GhRepo(classtree.Base):
 
         if data is None:
             data = github_request("GET", "/repos/{}/{}".format(owner, repo)).json()
+
         self._data = data
 
     def __getitem__(self, key):
         return self._data[key]
 
-GhRepo["issues"] = (GhRepoIssues, "_owner", "_repo")
+GhRepo["issues"] = GhRepoIssues
 
 
 class GhOwnerRepos(object):
@@ -84,11 +216,34 @@ class GhOwnerRepos(object):
         """
         self._owner = owner
 
+    def iterkeys(self):
+        """Enumerate repositories of the owner"""
+        repos = github_request_paginated("GET", "/users/{}/repos".format(self._owner))
+        return (x["name"] for x in repos)
+
+    __iter__ = iterkeys
+
+    def keys(self):
+        return list(self.iterkeys())
+
+    def itervalues(self):
+        return (x[1] for x in self.iteritems())
+
+    def values(self):
+        return list(self.itervalues())
+
+    def iteritems(self):
+        repos = github_request_paginated("GET", "/users/{}/repos".format(self._owner))
+        return ((x["name"], GhRepo(self._owner, x["name"], x)) for x in repos)
+
+    def items(self):
+        return list(self.iteritems())
+
     def __getitem__(self, repo):
         """Return the GhRepo object for `repo`
 
 Check if `repo` is a valid repo first.
-"""
+        """
         # Check if `repo` is a valid repo/user
         req = github_request("GET", "/repos/{}/{}".format(self._owner, repo))
         if "200" not in req.headers["status"]:
@@ -98,16 +253,32 @@ Check if `repo` is a valid repo first.
         # Return GhOwnerRepos object
         return GhRepo(self._owner, repo, req.json())
 
+    def __setitem__(self, repo, parameters):
+        if self._owner == config.get("github", "user"):
+            url = "/user/repos"
+        elif Github()["users"][self._owner]["type"] == "Organization":
+            url = "/orgs/{}/repos".format(self._owner)
+        else:
+            raise ValueError("Couldn't create repository: No permission.")
+
+        req = github_request("POST", url,
+                             data = merge_dicts(dict(name=repo), parameters))
+        if "201 Created" not in req.headers["status"]:
+            raise ValueError("Couldn't create repository: {}"
+                             .format(req.json()["message"]))
+
 
 class GhRepos(object):
     """Github repository container
     """
+    def __init__(self, parent):
+        self._parent = parent
 
     def __getitem__(self, owner):
         """Return the GhOwnerRepos object for `owner`
 
 Check if `owner` is a valid owner first.
-"""
+        """
         # Check if `owner` is a valid owner/user
         if "200" not in github_request("HEAD", "/users/{}".format(owner)).headers["status"]:
             raise KeyError("Owner '{}' does not exist.".format(owner))
--- src/tpv/github/tests/test_github.py
+++ src/tpv/github/tests/test_github.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import unittest
+import itertools
 
 from ..github import *
 
@@ -19,3 +20,37 @@ class TestGithub(unittest.TestCase):
         self.assertTrue(isinstance(issues, GhRepoIssues))
         self.assertEqual(issues._owner, "nixos")
         self.assertEqual(issues._repo, "nixpkgs")
+
+    def test_owner_repos_enumeration(self):
+        nixosreponames = ("nixpkgs", "nixops", "nix", "hydra")
+
+        nixosrepos = Github()["repos"]["nixos"]
+        self.assertTrue(isinstance(nixosrepos, GhOwnerRepos))
+        self.assertTrue(set(nixosrepos).issuperset(nixosreponames))
+
+        values = nixosrepos.itervalues()
+        self.assertTrue(isinstance(next(values), GhRepo))
+        self.assertTrue(set(v["full_name"] for v in values)
+                        .issuperset(("NixOS/" + x for x in nixosreponames)))
+
+        patchelf = next(itertools.dropwhile(lambda x: x[0] != "patchelf",
+                                            nixosrepos.iteritems()))
+        self.assertTrue(isinstance(patchelf[1], GhRepo))
+        self.assertEqual(patchelf[1]["full_name"], "NixOS/patchelf")
+
+
+    def test_github_request_paginated(self):
+        repos = github_request_paginated("GET", "/users/nixos/repos?per_page=2")
+        self.assertTrue(len(list(itertools.islice(repos, 5))) == 5)
+
+    def test_issues(self):
+        patchelf = Github()["repos"]["nixos"]["patchelf"]
+        issues = patchelf["issues"]
+        no_of_issues = len(issues)
+        self.assertTrue(no_of_issues >= 7)
+
+        open_issues = list(itertools.takewhile(lambda x: x["state"]=="open", issues.itervalues()))
+        self.assertTrue(len(open_issues) <= no_of_issues)
+
+    def test_complete_data(self):
+        pass
