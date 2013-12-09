36ba01588c35ec512b103f65b11a05044b269d82
Jonas Hoersch <jonas@chaoflow.net>
2013-12-09 17:11:28 +0100

    issues: add trivial listcompletions

--- src/tpv/github/cli/issues.py
+++ src/tpv/github/cli/issues.py
@@ -16,22 +16,39 @@ class Issues(tpv.cli.Command):
 
 @add_argument_switches([
     dict(name="milestone", help=u"milestone number or '*' for any"),
-    dict(name="state", help=u"Indicates the state of the issues to return. Can be either open or closed.", completion=ListCompletion("open","close")),
+    dict(name="state", help=u"Indicates the state of the issues to return. Can be either open or closed.", completion=ListCompletion("open","closed")),
     dict(name="assignee", help=u"Can be the name of a user. Pass in none for issues with no assigned user, and * for issues assigned to any user."),
     dict(name="creator", help=u"The user that created the issue"),
     dict(name="mentioned", help=u"A user that's mentioned in the issue"),
     dict(name="labels", help=u"A list of comma separated label names"),
     dict(name="sort", help=u"What to sort results by. Can be either created, updated, comments."),
-    dict(name="direction", help=u"The direction of the sort. Can be either asc or desc."),
+    dict(name="direction", help=u"The direction of the sort. Can be either asc or desc.", completion=ListCompletion("asc","desc")),
     dict(name="since", help=u"Only issues updated at or after this time are returned (YYYY-MM-DDTHH:MM:SSZ).")
 ])
 @stdout_to_pager
 class List(tpv.cli.Command):
     """List issues matching filter criteria
+
+The use of the stdout_to_pager aspect confuses plumbum into thinking
+we accept a list of nondescript arguments, which blocks proper
+completion of the options.
+
     """
     repo = tpv.cli.SwitchAttr("--repo", str,
                               help="The repository <user>/<repo>",
                               completion=RepositoryDynamicCompletion())
+
+    mine = tpv.cli.SwitchAttr("--mine", str,
+                              help="List my issues",
+                              completion=ListCompletion("all",
+                                                        "assigned",
+                                                        "created",
+                                                        "mentioned",
+                                                        "subscribed"),
+                              excludes=("--milestone",
+                                        "--assignee",
+                                        "--creator",
+                                        "--mentioned"))
 
     def __init__(self, *args):
         tpv.cli.Command.__init__(self, *args)
@@ -145,7 +162,8 @@ class Add(tpv.cli.Command):
     dict(name="assignee",
          help=u"Login for the user that this issue should be assigned to."),
     dict(name="state",
-         help=u"State of the issue (open/closed)."),
+         help=u"State of the issue (open/closed).",
+         completion=ListCompletion("open", "closed")),
     dict(name="milestone",
          help=u"Milestone to associate this issue with."),
     dict(name="labels", flagname="--label", list=True,
